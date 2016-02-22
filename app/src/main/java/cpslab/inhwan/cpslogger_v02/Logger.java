ed26d0f9e04a8931a7c4b2367a5d2b8650dbfa28
hyunjun <kiki9014@gmail.com>
2016-02-22 16:19:42 +0900

    Add Logger.java =>Copy from LoggingService.java Modified LoggingService.java =>It can be deleted later

---
+++ app/src/main/java/cpslab/inhwan/cpslogger_v02/Logger.java
@@ -0,0 +1,65 @@
+package cpslab.inhwan.cpslogger_v02;
+
+/**
+ * Created by Hyunjun on 2/22/2016.
+ */
+
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+    String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+    String currentDate;
+    File file;
+    FileOutputStream fileOutputStream;
+    OutputStreamWriter outputStreamWriter;
+
+    Logger(String name){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy_mm_dd");
+        currentDate = simpleDateFormat.format(date);
+
+        createFile(name);
+    }
+
+    public void createFile(String name){
+        String fileName = "CPSLogger_" + name + "_" + currentDate + ".csv";
+        String filePath = baseDir + File.pathSeparator + "CPSLogger" + File.separator+fileName;
+
+        File file = new File(filePath);
+
+        try{
+            fileOutputStream = new FileOutputStream(file);
+            outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        }
+        catch (Exception e){
+            Log.e("Logging", e.toString());
+        }
+    }
+
+    public void writeData(String name, String data){
+        SimpleDateFormat format = new SimpleDateFormat("kk:mm:ss.SSS");
+        String timeStamp = format.format(new Date());
+
+        try{
+            outputStreamWriter.write(timeStamp + "," + data);
+        }
+        catch (Exception e){
+            Log.e("Logging", e.toString());
+        }
+    }
+
+    public void closeFile(String name){
+        try{
+            outputStreamWriter.close();
+            fileOutputStream.close();
+        }
+        catch (Exception e){
+            Log.e("Logging", e.toString());
+        }
+    }
+}
--- app/src/main/java/cpslab/inhwan/cpslogger_v02/LoggingService.java
+++ app/src/main/java/cpslab/inhwan/cpslogger_v02/LoggingService.java
@@ -1,6 +1,8 @@
 package cpslab.inhwan.cpslogger_v02;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
@@ -85,5 +87,12 @@ public class LoggingService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public class saveReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent){
+            
+        }
     }
 }
