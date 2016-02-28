0557d944764b29084084d4f48572d8f97fa44adf
hyunjun <kiki9014@gmail.com>
2016-02-29 04:29:44 +0900

    add text input activity

---
+++ app/src/main/java/cpslab/inhwan/cpslogger_v02/textInput.java
@@ -0,0 +1,13 @@
+package cpslab.inhwan.cpslogger_v02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class textInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_input);
+    }
+}
